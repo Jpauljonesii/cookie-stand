6c8a5d69df9c8b9ca7652599d1b9d4e5114cf344
John P. Jones II <jpauljonesii@gmail.com>
2023-06-18 21:45:36 -0500

    created files for lab 6 and updated basic html and javascript

---
+++ app.js
@@ -0,0 +1,5 @@
+'use strict';
+let username = prompt('What is your name?');
+let greeting = document.querySelector ('.greeting');
+greeting.textContent= `Hi ${username} welcome to Pat's Salmon Cookies`;
+

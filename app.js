1422e04f695b44bc0a97d8f7709e0bdde4b3b5f9
John P. Jones II <jpauljonesii@gmail.com>
2023-06-28 22:52:55 -0500

    completed lab 9

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-const hoursOpen = ['','6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', ''];
+const hoursOpen = ['','6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', ''];
 
 function City(location, minCust, maxCust, avgSale) {
   this.location = location;
@@ -11,7 +11,7 @@ City.prototype.randomNumberOfCustomers = function (){
   return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.maxCust);
 };
 
-let main = document.getElementById('cities');
+let main = document.querySelector('#cities');
 let table = document.createElement('table');
 main.appendChild(table);
 let Seattle = new City('Seattle', 23, 65, 6.3);
@@ -19,7 +19,7 @@ let Tokyo = new City('Tokyo', 3, 24, 1.2);
 let Dubai = new City('Dubai', 11, 38, 3.7);
 let Paris = new City('Paris', 20, 38, 2.3);
 let Lima = new City('Lima', 2, 16, 4.6);
-
+const allCities = [Seattle, Tokyo, Dubai, Paris, Lima];
 let headerRow = document.createElement('tr');
 table.appendChild(headerRow);
 let tableBody = document.createElement('tbody');
@@ -34,7 +34,25 @@ for (let i = 0; i < hoursOpen.length - 1; i++) {
     headerRow.appendChild(totalTh);
   }
 }
+function addLocation(){
+  let form = document.querySelector('#newCookieStandForm');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    event.stopPropagation();
 
+    const newCookieLocation = event.target.location.value;
+    const newCookieMaxCust = event.target.maxCust.value;
+    const newCookieMinCust = event.target.minCust.value;
+    const newCookieAvgSale = event.target.avgSale.value;
+    let addLocation = new City (
+      newCookieLocation,
+      newCookieMinCust,newCookieMaxCust, newCookieAvgSale
+    );
+    addLocation.render();
+    newFooterFunction();
+  });
+}
+addLocation();
 const cityArray = [];
 City.prototype.render = function () {
   let totalSales = 0;
@@ -53,9 +71,9 @@ City.prototype.render = function () {
     td.innerText = hourlySales;
     cityDataRow.appendChild(td);
     totalSales += hourlySales;
-    if (i === 14) {
+    if (i === 13) {
       let totalTh = document.createElement('td');
-      totalTh.innertext = totalSales;
+      totalTh.innerText = totalSales;
       cityDataRow.appendChild(totalTh);
     }
   }
@@ -70,8 +88,8 @@ function footerFunction() {
   let tr = document.createElement('tr');
   tfoot.appendChild(tr);
   tr.appendChild(td);
-  const totalOfTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  for (let i = 0; i < 15; i++){
+  const totalOfTotals = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (let i = 0; i < 14; i++){
     for (let j = 0; j < cityArray.length; j++){
       totalOfTotals[i] += cityArray[j][i];
     }
@@ -89,9 +107,17 @@ function footerFunction() {
   tr.appendChild(td);
 }
 
-Seattle.render();
-Tokyo.render();
-Dubai.render();
-Paris.render();
-Lima.render();
+for (let i = 0; i < allCities.length; i++){
+  allCities[i].render();
+}
+/*
+Added function to add new footer for new location from form
+Worked on this function with Jon Young
+*/
+function newFooterFunction (){
+  let tfoot = document.querySelector('tfoot');
+  tfoot.remove();
+  footerFunction();
+}
 footerFunction();
+
